8cdfbcc1309c5b6defbf328da289a89f4e7cbea9
Jean Huang <jeanettecyhuang@gmail.com>
2019-06-20 20:59:09 +0800

    second addtion

--- guss.py
+++ guss.py
@@ -1,6 +1,8 @@
 import random
 r = random.randint(1,100)
+count = 0
 while True :
+	count += 1
 	num = input('請猜數字: ')
 	num =int(num)
 	if num == r:
@@ -10,4 +12,5 @@ while True :
 		print('比答案小')
 	elif num > r:
 		print('比答案大')
+	print('這是你猜的第',count,'次')
 
